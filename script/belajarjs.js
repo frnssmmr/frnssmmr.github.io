bdbc996a3878bda33929451b653776f97aaa0e52
frnssmmr <105479006+frnssmmr@users.noreply.github.com>
2022-05-27 15:09:09 +0700

    Add files via upload

---
+++ script/belajarjs.js
@@ -0,0 +1,16 @@
+// let favFood = 'Saksang';
+// if (favFood == 'Saksang') {
+//     alert('Ya saya sangat suka saksang :)');
+// }
+// else {
+//     alert('noww, saya suka saksang :)')
+// }
+
+function perkalian (ang1,ang2){
+    let hasil  = (ang1*ang2);
+    return hasil;
+}
+
+perkalian (2,3);
+perkalian(59,2);
+perkalian(22*44);
---
+++ script/scripts.js
@@ -0,0 +1,59 @@
+function tombolYes() {
+    document.getElementById("pic1").style.display="inline";
+    document.getElementById("jdlbsr").style.display="none";
+    document.getElementById("question").style.display="none";
+    document.getElementById("no1").style.display="none";
+    document.getElementById("yes1").style.display="none";
+    return;
+}
+
+function tombolYes2() {
+    document.getElementById("pic2").style.display="inline";
+    document.getElementById("pic2").style.width="500px";
+    document.getElementById("jdlbsr").style.display="none";
+    document.getElementById("ygbener").style.display="none";
+    document.getElementById("yakin").style.display="none";
+    document.getElementById("question").style.display="none;";
+    document.getElementById("no2").style.display="none";
+    document.getElementById("no3").style.display="none";
+    document.getElementById("yes2").style.display="none";
+    return;
+}
+
+function tombolNo() {
+    
+    document.getElementById("yakin").innerHTML = "Yakin Dek?";
+    document.getElementById("question").style.display = "none";
+    document.getElementById("yes1").style.display ="none";
+    document.getElementById("no1").style.display = "none";
+    document.getElementById("yes2").style.display = "inline";
+    document.getElementById("no2").style.display = "inline";
+    return;
+}
+
+function tombolNo2() {
+    document.getElementById("yakin").style.display = "none";
+    document.getElementById("ygbener").innerHTML ="Yang Benerrr ?";
+    document.getElementById("no2").style.display = "none";
+    document.getElementById("no3").style.display = "inline";
+    return;
+
+}
+function tombolNo3() {
+    document.getElementById("gif1").style.display="inline";
+    document.getElementById("jdlbsr").style.display="none";
+    document.getElementById("ygbener").style.display="none";
+    document.getElementById("yakin").style.display="none";
+    document.getElementById("question").style.display="none;";
+    document.getElementById("no2").style.display="none";
+    document.getElementById("no3").style.display="none";
+    document.getElementById("yes2").style.display="none";
+    document.getElementById("yes3").style.display="inline";
+    return;
+}
+
+function tombolYes3() {
+    document.getElementById("gif2").style.display ="inline";
+    document.getElementById("gif1").style.display ="none";
+    document.getElementById("yes3").style.display ="none";
+}
